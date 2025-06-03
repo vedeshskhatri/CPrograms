ad1bf2c959f1a427466cdffc0bc415274892f2f4
Vedesh S Khatri <158317352+vedeshskhatri@users.noreply.github.com>
2025-06-03 11:13:49 +0530

    Add files via upload

---
+++ 03_goodbye.cpp
@@ -0,0 +1,10 @@
+// Goodbye ✌️
+// Codédex
+
+#include <iostream> 
+
+int main() {
+  std::cout << "See ya later, alligator!\n"; 
+  std::cout << "さよなら\n";
+  std::cout << "Au revoir\n";
+}
---
+++ 04_ouija.cpp
@@ -0,0 +1,15 @@
+// Ouija Board 👻
+// Codédex
+
+#include <iostream> 
+
+int main() {
+  std::cout << "-----------------------------------\n";
+  std::cout << "|          x YES   NO  x          |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|         ABCDEFGHIJKLM           |\n";
+  std::cout << "|         NOPQRSTUVWXYZ           |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|           1234567890            |\n";
+  std::cout << "-----------------------------------\n";
+}
